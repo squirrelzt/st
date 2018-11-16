c9551c9c0c90f19a3e2fc9b582f94ec12af8f8e4
squirrelzt <squirrelzt@126.com>
2018-11-16 13:12:56 +0800

    添加lombok依赖包，添加slf4j日志

--- src/main/java/com/squirrel/st/StApplication.java
+++ src/main/java/com/squirrel/st/StApplication.java
@@ -1,12 +1,15 @@
 package com.squirrel.st;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class StApplication {
 
 	public static void main(String[] args) {
+		LOGGER.error("------------------");
 		SpringApplication.run(StApplication.class, args);
 	}
 }
