464553106c4e9b4684d8dfe806af2fe45d5c9832
squirrelzt <squirrelzt@126.com>
2018-11-16 09:01:01 +0800

    spring boot 2.1.0 init

---
+++ src/main/java/com/squirrel/st/StApplication.java
@@ -0,0 +1,12 @@
+package com.squirrel.st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StApplication.class, args);
+	}
+}
