3f0b55224e531b9613e28e1790beaccf510606c3
squirrelzt <squirrelzt@126.com>
2019-04-01 16:33:12 +0800

    java8格式化日期(配置类)

--- annotation/src/main/java/com/annotation/controller/DateController.java
+++ annotation/src/main/java/com/annotation/controller/DateController.java
@@ -4,10 +4,18 @@ import com.annotation.vo.DateVO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 类名称: DateController
+ * 类描述: 日期格式化
+ * @author squirrel
+ * @date 2019-04-01
+ */
 @RestController
 @RequestMapping("/date")
 public class DateController {
@@ -15,10 +23,24 @@ public class DateController {
     @RequestMapping("/now")
     public Map<String, Object> getNow() {
         Map<String, Object> result = new HashMap<>(2);
+
         DateVO vo = new DateVO();
+        LocalDate localDate = LocalDate.now();
+        LocalDateTime localDateTime = LocalDateTime.now();
+
         vo.setGmtCreate(new Date());
         vo.setGmtModified(new Date());
+
+        vo.setGmtCreate8(localDate);
+        vo.setGmtModified8(localDate);
+
+        vo.setGmtCreateTime(localDateTime);
+        vo.setGmtModifiedTime(localDateTime);
+
         result.put("now", vo);
+
+
+
         return result;
     }
 }
--- annotation/src/main/java/com/annotation/vo/DateVO.java
+++ annotation/src/main/java/com/annotation/vo/DateVO.java
@@ -2,8 +2,16 @@ package com.annotation.vo;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
+/**
+ * 类名称: DateVO
+ * 类描述: 日期VO
+ * @squirrel
+ * @date 2019-04-01
+ */
 @Data
 public class DateVO {
 
@@ -16,4 +24,24 @@ public class DateVO {
      * 修改时间
      */
     private Date gmtModified;
+
+    /**
+     * 创建时间(Java8)
+     */
+    private LocalDate gmtCreate8;
+
+    /**
+     * 修改时间(Java8)
+     */
+    private LocalDate gmtModified8;
+
+    /**
+     * 创建时间(Java8)
+     */
+    private LocalDateTime gmtCreateTime;
+
+    /**
+     * 修改时间(Java8)
+     */
+    private LocalDateTime gmtModifiedTime;
 }
