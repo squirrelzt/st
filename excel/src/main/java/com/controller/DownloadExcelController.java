94c9df1ef1db61f7bce87f4f858ffff5ae63ffed
squirrelzt <squirrelzt@126.com>
2019-08-23 17:01:18 +0800

    update

--- excel/src/main/java/com/controller/DownloadExcelController.java
+++ excel/src/main/java/com/controller/DownloadExcelController.java
@@ -2,12 +2,12 @@ package com.controller;
 
 import com.alibaba.excel.EasyExcelFactory;
 import com.alibaba.excel.ExcelWriter;
-import com.alibaba.excel.metadata.BaseRowModel;
 import com.alibaba.excel.metadata.Font;
 import com.alibaba.excel.metadata.Sheet;
 import com.alibaba.excel.metadata.TableStyle;
 import com.alibaba.excel.support.ExcelTypeEnum;
 import com.domain.TruckDriver;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,66 +16,62 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
+/**
+ * 类名称: DownloadExcelController
+ * 类描述: 下载Excel
+ */
+@Slf4j
 @RestController
 @RequestMapping("/download")
 public class DownloadExcelController {
 
     @RequestMapping("/excel1")
-    public void downloadExcel1(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        ServletOutputStream out = response.getOutputStream();
-        response.setContentType("multipart/form-data");
-        response.setCharacterEncoding("utf-8");
-        response.setHeader("Content-disposition", "attachment;filename=test.xlsx");
-        ExcelWriter writer = new ExcelWriter(out, ExcelTypeEnum.XLSX, false);
-//        String fileName = new String(("UserInfo " + new SimpleDateFormat("yyyy-MM-dd").format(new Date()))
-//                .getBytes(), "UTF-8");
-        Sheet sheet1 = new Sheet(1, 1);
-        sheet1.setSheetName("sheet11");
-        writer.write0(getListString(), sheet1);
-
-//        List<? extends BaseRowModel> data = getData();
-//        writer.write(data, sheet1);
-        writer.finish();
-
-        out.flush();
+    public void downloadExcel1(HttpServletRequest request, HttpServletResponse response) {
+        try (ServletOutputStream out = response.getOutputStream()) {
+            response.setContentType("multipart/form-data");
+            response.setCharacterEncoding("utf-8");
+            response.setHeader("Content-disposition", "attachment;filename=test.xlsx");
+            ExcelWriter writer = new ExcelWriter(out, ExcelTypeEnum.XLSX, false);
+            Sheet sheet1 = new Sheet(1, 1);
+            sheet1.setSheetName("sheet11");
+            writer.write0(getListString(), sheet1);
+            writer.finish();
+            out.flush();
+        } catch (IOException e) {
+            log.error("下载Excel失败", e);
+        }
     }
 
     @RequestMapping("/excel")
-    public void downloadExcel(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        ServletOutputStream out = response.getOutputStream();
-        response.setContentType("multipart/form-data");
-        response.setCharacterEncoding("utf-8");
-        response.setHeader("Content-disposition", "attachment;filename=test.xlsx");
-//        ExcelWriter writer = new ExcelWriter(out, ExcelTypeEnum.XLSX, false);
-        ExcelWriter writer = EasyExcelFactory.getWriter(out);
-//        String fileName = new String(("UserInfo " + new SimpleDateFormat("yyyy-MM-dd").format(new Date()))
-//                .getBytes(), "UTF-8");
-        Sheet sheet1 = new Sheet(1, 0, TruckDriver.class);
-        TableStyle tableStyle = new TableStyle();
-        tableStyle.setTableHeadBackGroundColor(IndexedColors.YELLOW);
-        Font font = new Font();
-        font.setFontHeightInPoints((short)12);
-        tableStyle.setTableContentFont(font);
-        Font headFont = new Font();
-        headFont.setBold(true);
-        headFont.setFontHeightInPoints((short)10);
-        tableStyle.setTableHeadFont(headFont);
-        tableStyle.setTableContentBackGroundColor(IndexedColors.WHITE);
-        sheet1.setTableStyle(tableStyle);
-        sheet1.setSheetName("sheet");
-//        sheet1.setHeadLineMun();
-//        writer.write0(getListString(), sheet1);
-
-        List<TruckDriver> data = getData();
-        writer.write(data, sheet1);
-        writer.finish();
-
-        out.flush();
+    public void downloadExcel(HttpServletRequest request, HttpServletResponse response) {
+        try (ServletOutputStream out = response.getOutputStream()) {
+            response.setContentType("multipart/form-data");
+            response.setCharacterEncoding("utf-8");
+            response.setHeader("Content-disposition", "attachment;filename=test.xlsx");
+            ExcelWriter writer = EasyExcelFactory.getWriter(out);
+            Sheet sheet = new Sheet(1, 0, TruckDriver.class);
+            TableStyle tableStyle = new TableStyle();
+            tableStyle.setTableHeadBackGroundColor(IndexedColors.YELLOW);
+            Font font = new Font();
+            font.setFontHeightInPoints((short)12);
+            tableStyle.setTableContentFont(font);
+            Font headFont = new Font();
+            headFont.setBold(Boolean.TRUE);
+            headFont.setFontHeightInPoints((short)10);
+            tableStyle.setTableHeadFont(headFont);
+            tableStyle.setTableContentBackGroundColor(IndexedColors.WHITE);
+            sheet.setTableStyle(tableStyle);
+            sheet.setSheetName("sheet");
+            List<TruckDriver> data = getData();
+            writer.write(data, sheet);
+            writer.finish();
+            out.flush();
+        } catch (IOException e) {
+            log.error("下载Excel失败", e);
+        }
     }
 
     private List<List<String>> getListString() {
